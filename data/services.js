642a1a9f26bf91e079177920c8def47c2f671151
Malikiyaw <reynerabdon14@gmail.com>
2026-08-18 01:23:25 +0800

    Add structured Chamomile demo service data

---
+++ data/services.js
@@ -0,0 +1,6 @@
+window.CHAMOMILE_SERVICES=[
+ {category:'Hair',items:[['Blow Dry','Professional blow-dry styling','N/A','N/A'],['Hair Styling','Styling tailored to your look','N/A','N/A'],['Hair Coloring','Color services and finishing','N/A','N/A'],['Haircut','Haircut and finishing service','N/A','N/A'],['Hair Treatment','Care-focused hair treatment','N/A','N/A']]},
+ {category:'Nails',items:[['Manicure','Classic manicure care','N/A','N/A'],['Pedicure','Classic pedicure care','N/A','N/A'],['Gel Nails','Gel nail service','N/A','N/A'],['Nail Add-ons','Additional nail details','N/A','N/A']]},
+ {category:'Brows & Lashes',items:[['Brows','Brow care and shaping','N/A','N/A'],['Lashes','Lash beauty service','N/A','N/A'],['Lifting Services','Lash or brow lifting service','N/A','N/A']]},
+ {category:'Beauty & Treatments',items:[['Beauty Treatment','Beauty care service','N/A','N/A'],['Relaxation Treatment','Restorative salon treatment','N/A','N/A']]}
+];
